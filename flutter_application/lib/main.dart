6a55200d1cc9b716167f3d330d546d3c3629dbf5
haoyiwen <yw.hao@trip.com>
2023-11-04 21:17:16 +0800

    feat: add stawberry pavlova

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
@@ -11,10 +9,138 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text(
-      'Hello, world!',
-      textDirection: TextDirection.ltr,
-    ));
+    return const MaterialApp(
+        title: "flutter layout demo",
+        home: Scaffold(
+          body: Row(
+            children: [
+              SizedBox(width: 400, child: LeftColumn()),
+              RightColumn()
+            ],
+          ),
+        ));
+  }
+}
+
+class LeftColumn extends StatelessWidget {
+  const LeftColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+          margin: const EdgeInsets.all(20),
+          child: const Text("Stawberry Pavlova",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))),
+      const Text(
+        "Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.",
+        textAlign: TextAlign.center,
+      ),
+      const SubTitle(),
+      Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: const Text("PREP:"),
+                ),
+                const Text("25 min", style: TextStyle(fontWeight: FontWeight.bold))
+              ],
+            ),
+            const SizedBox(
+              width: 40,
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: const Text("COOK:"),
+                ),
+                const Text("1 hr", style: TextStyle(fontWeight: FontWeight.bold))
+              ],
+            ),
+            const SizedBox(
+              width: 40,
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: const Text("FEEDS:"),
+                ),
+                const Text("4-6", style: TextStyle(fontWeight: FontWeight.bold))
+              ],
+            )
+          ],
+        ),
+      )
+    ]);
+  }
+}
+
+class SubTitle extends StatelessWidget {
+  const SubTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.star,
+            color: Colors.black,
+            size: 20,
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.black,
+            size: 20,
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.black,
+            size: 20,
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.black,
+            size: 20,
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.black,
+            size: 20,
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.only(left: 10),
+            child: const Text(
+              "170 Reviews",
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RightColumn extends StatelessWidget {
+  const RightColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Image.asset("assets/pavlova.jpg", fit: BoxFit.cover));
   }
 }
