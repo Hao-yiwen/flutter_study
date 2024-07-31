48b3e6a649b41f86e107e41573c5e0b16f853307
Hao-yiwen <1552975589@qq.com>
2024-07-31 23:40:23 +0800

    feat: add lsolate test page

--- flutter_application/lib/HomePage.dart
+++ flutter_application/lib/HomePage.dart
@@ -10,147 +10,150 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('大首页'),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: Column(children: [
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/sqliteDemo')},
-                  child: const Text('Sqlite Demo')),
-              TextButton(
-                child: const Text('TimerTestPage'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: ListView(children: [
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/sqliteDemo')},
+                child: const Text('Sqlite Demo')),
+            TextButton(
+              child: const Text('TimerTestPage'),
+              onPressed: () => {
+                GoRouter.of(context).go('/TimerTestPage'),
+              },
+            ),
+            TextButton(
                 onPressed: () => {
-                  GoRouter.of(context).go('/TimerTestPage'),
-                },
-              ),
-              TextButton(
-                  onPressed: () => {
-                        GoRouter.of(context).go('/CameraPage'),
-                      },
-                  child: const Text('CameraPage')),
-              TextButton(
-                  onPressed: () => {
-                        GoRouter.of(context).go('/LocalizationPage'),
-                      },
-                  child: const Text('LocalizationPage')),
-              TextButton(
-                child: const Text('SharedPreferencesPage'),
+                      GoRouter.of(context).go('/CameraPage'),
+                    },
+                child: const Text('CameraPage')),
+            TextButton(
+                onPressed: () => {
+                      GoRouter.of(context).go('/LocalizationPage'),
+                    },
+                child: const Text('LocalizationPage')),
+            TextButton(
+              child: const Text('SharedPreferencesPage'),
+              onPressed: () =>
+                  {GoRouter.of(context).go('/SharedPreferencesPage')},
+            ),
+            TextButton(
+              child: const Text('LifecyclePage'),
+              onPressed: () => {GoRouter.of(context).go('/LifecyclePage')},
+            ),
+            TextButton(
+              child: const Text('TabBarPage'),
+              onPressed: () => {GoRouter.of(context).go('/TabBarPage')},
+            ),
+            TextButton(
+              child: const Text('DrawerPage'),
+              onPressed: () => {GoRouter.of(context).go('/DrawerPage')},
+            ),
+            TextButton(
+              child: const Text('GesturePage'),
+              onPressed: () => {GoRouter.of(context).go('/GesturePage')},
+            ),
+            TextButton(
+              child: const Text('HttpPage'),
+              onPressed: () => {GoRouter.of(context).go('/HttpPage')},
+            ),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/FormPage')},
+                child: const Text('FormPage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/PlatformPage')},
+                child: const Text('PlatformPage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/IconPage')},
+                child: const Text('IconPage')),
+            TextButton(
                 onPressed: () =>
-                    {GoRouter.of(context).go('/SharedPreferencesPage')},
-              ),
-              TextButton(
-                child: const Text('LifecyclePage'),
-                onPressed: () => {GoRouter.of(context).go('/LifecyclePage')},
-              ),
-              TextButton(
-                child: const Text('TabBarPage'),
-                onPressed: () => {GoRouter.of(context).go('/TabBarPage')},
-              ),
-              TextButton(
-                child: const Text('DrawerPage'),
-                onPressed: () => {GoRouter.of(context).go('/DrawerPage')},
-              ),
-              TextButton(
-                child: const Text('GesturePage'),
-                onPressed: () => {GoRouter.of(context).go('/GesturePage')},
-              ),
-              TextButton(
-                child: const Text('HttpPage'),
-                onPressed: () => {GoRouter.of(context).go('/HttpPage')},
-              ),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/FormPage')},
-                  child: const Text('FormPage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/PlatformPage')},
-                  child: const Text('PlatformPage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/IconPage')},
-                  child: const Text('IconPage')),
-              TextButton(
-                  onPressed: () =>
-                      {GoRouter.of(context).go('/BoxDecorationPage')},
-                  child: const Text('BoxDecorationPage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/ShoppingList')},
-                  child: const Text('ShoppingList')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/LayoutPage')},
-                  child: const Text('LayoutPage')),
-              TextButton(
-                  onPressed: () =>
-                      {GoRouter.of(context).go('/CupertinoButtonPage')},
-                  child: const Text('CupertinoButtonPage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/ThemePage')},
-                  child: const Text('ThemePage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/Fontfamilypage')},
-                  child: const Text('Fontfamilypage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/GridPage')},
-                  child: const Text('GridPage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/MyPluginPage')},
-                  child: const Text('MyPluginPage')),
-              TextButton(
-                child: const Text('SqlitePage2'),
-                onPressed: () => {GoRouter.of(context).go('/SqlitePage2')},
-              ),
-              TextButton(
-                child: const Text('NavigationPage'),
-                onPressed: () => {GoRouter.of(context).go('/NavigationPage')},
-              ),
-              TextButton(
-                child: const Text('GuessGamePage'),
-                onPressed: () => {GoRouter.of(context).go('/GuessGamePage')},
-              ),
-              TextButton(
-                child: const Text('RandomWords'),
-                onPressed: () => {GoRouter.of(context).go('/RandomWords')},
-              ),
-              TextButton(
-                child: const Text('Lottiepage'),
-                onPressed: () => {GoRouter.of(context).go('/Lottiepage')},
-              ),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/GetXpage')},
-                  child: const Text('GetXpage')),
-              TextButton(
-                  onPressed: () => {GoRouter.of(context).go('/WebViewPage')},
-                  child: const Text('WebViewPage')),
-              TextButton(
-                  onPressed: () =>
-                      {GoRouter.of(context).go('/Inheritedtestpage')},
-                  child: Text("Inheritedtestpage")),
-              TextButton(
-                  onPressed: () {
-                    GoRouter.of(context).go('/MypainterPage');
-                  },
-                  child: Text("MypainterPage")),
-              TextButton(
-                  onPressed: () {
-                    GoRouter.of(context).go('/Lifecycletestpage');
-                  },
-                  child: Text("Lifecycletestpage")),
-              TextButton(
-                  onPressed: () {
-                    GoRouter.of(context).go('/FadeAppTestPage');
-                  },
-                  child: Text("FadeAppTestPage")),
-              TextButton(
-                  onPressed: () {
-                    GoRouter.of(context).go('/RedBoxPage');
-                  },
-                  child: Text("RedBoxPage")),
-              TextButton(
-                  onPressed: () {
-                    GoRouter.of(context).go('/RotatingBoxPage');
-                  },
-                  child: Text("RotatingBoxPage")),
-            ]),
-          ),
+                    {GoRouter.of(context).go('/BoxDecorationPage')},
+                child: const Text('BoxDecorationPage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/ShoppingList')},
+                child: const Text('ShoppingList')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/LayoutPage')},
+                child: const Text('LayoutPage')),
+            TextButton(
+                onPressed: () =>
+                    {GoRouter.of(context).go('/CupertinoButtonPage')},
+                child: const Text('CupertinoButtonPage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/ThemePage')},
+                child: const Text('ThemePage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/Fontfamilypage')},
+                child: const Text('Fontfamilypage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/GridPage')},
+                child: const Text('GridPage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/MyPluginPage')},
+                child: const Text('MyPluginPage')),
+            TextButton(
+              child: const Text('SqlitePage2'),
+              onPressed: () => {GoRouter.of(context).go('/SqlitePage2')},
+            ),
+            TextButton(
+              child: const Text('NavigationPage'),
+              onPressed: () => {GoRouter.of(context).go('/NavigationPage')},
+            ),
+            TextButton(
+              child: const Text('GuessGamePage'),
+              onPressed: () => {GoRouter.of(context).go('/GuessGamePage')},
+            ),
+            TextButton(
+              child: const Text('RandomWords'),
+              onPressed: () => {GoRouter.of(context).go('/RandomWords')},
+            ),
+            TextButton(
+              child: const Text('Lottiepage'),
+              onPressed: () => {GoRouter.of(context).go('/Lottiepage')},
+            ),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/GetXpage')},
+                child: const Text('GetXpage')),
+            TextButton(
+                onPressed: () => {GoRouter.of(context).go('/WebViewPage')},
+                child: const Text('WebViewPage')),
+            TextButton(
+                onPressed: () =>
+                    {GoRouter.of(context).go('/Inheritedtestpage')},
+                child: Text("Inheritedtestpage")),
+            TextButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/MypainterPage');
+                },
+                child: Text("MypainterPage")),
+            TextButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/Lifecycletestpage');
+                },
+                child: Text("Lifecycletestpage")),
+            TextButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/FadeAppTestPage');
+                },
+                child: Text("FadeAppTestPage")),
+            TextButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/RedBoxPage');
+                },
+                child: Text("RedBoxPage")),
+            TextButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/RotatingBoxPage');
+                },
+                child: Text("RotatingBoxPage")),
+            TextButton(
+                onPressed: () {
+                  GoRouter.of(context).go('/IsolateTestPage');
+                },
+                child: Text("IsolateTestPage")),
+          ]),
         ),
       ),
     );
