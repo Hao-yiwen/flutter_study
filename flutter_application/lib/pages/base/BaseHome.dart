f6426573c6b7b0dd6aaf06fb682b7ef60288a38f
yw.hao <yw.hao@trip.com>
2024-08-07 19:28:12 +0800

    feat: add navigation

--- flutter_application/lib/pages/base/BaseHome.dart
+++ flutter_application/lib/pages/base/BaseHome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/pages/base/model/PostData.dart';
 import 'package:go_router/go_router.dart';
 
 class BaseHome extends StatelessWidget {
@@ -13,14 +14,30 @@ class BaseHome extends StatelessWidget {
       body: ListView(
         children: [
           TextButton(
-              onPressed: () => {GoRouter.of(context).go('/BaseHome/ImagePlaceholder')},
+              onPressed: () =>
+                  {GoRouter.of(context).go('/BaseHome/ImagePlaceholder')},
               child: const Text('ImagePlaceholder')),
           TextButton(
-              onPressed: () => {GoRouter.of(context).go('/BaseHome/VideoPlayerScreen')},
+              onPressed: () =>
+                  {GoRouter.of(context).go('/BaseHome/VideoPlayerScreen')},
               child: const Text('VideoPlayerScreen')),
           TextButton(
-              onPressed: () => {GoRouter.of(context).go('/BaseHome/SvgTransformPage')},
+              onPressed: () =>
+                  {GoRouter.of(context).go('/BaseHome/SvgTransformPage')},
               child: const Text('SvgTransformPage')),
+          TextButton(
+              onPressed: () async {
+                final String str = await GoRouter.of(context).push(
+                        '/BaseHome/GoRouterExtradata',
+                        extra: PostData(
+                            title: "hello",
+                            url: "http://www.baidu.com")) as String ??
+                    '';
+                print('返回值：$str');
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('返回值：' + str)));
+              },
+              child: const Text('GoRouterExtradata')),
         ],
       ),
     );
